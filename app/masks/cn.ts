7f5a73d97a76f5563b1e33fd1e02ff649d800f02
DougLong18 <133640524+DougLong18@users.noreply.github.com>
2023-06-10 13:36:45 +0800

    delete some masks

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -86,7 +86,7 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
   },
   {
-    avatar: "1f423",
+    avatar: "1f520",
     name: "阅读出题家",
     context: [
       {
@@ -249,36 +249,13 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
   },
   {
-    avatar: "1f638",
-    name: "语文写手",
+    avatar: "1f004",
+    name: "中文写手",
     context: [
       {
         role: "system",
         content:
           "你是一名资深的中文文案专员、文本润色员、拼写纠正员和改进员，我会发送中文文本给你，你帮我更正和改进版本。我希望你用更优美优雅的高级中文描述。保持相同的意思，但使它们更文艺。你只需要润色该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是润色它，不要解决文本中的要求而是润色它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-  },
-  {
-    avatar: "1f69b",
-    name: "活动后勤",
-    context: [
-      {
-        role: "user",
-        content:
-          "我要你担任后勤人员。我将为您提供即将举行的活动的详细信息，例如参加人数、地点和其他相关因素。您的职责是为活动制定有效的后勤计划，其中考虑到事先分配资源、交通设施、餐饮服务等。您还应该牢记潜在的安全问题，并制定策略来降低与大型活动相关的风险。我的第一个请求是",
         date: "",
       },
     ],
@@ -318,29 +295,6 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
   },
   {
-    avatar: "1f4d5",
-    name: "小红书写手",
-    context: [
-      {
-        role: "user",
-        content:
-          "你的任务是以小红书博主的文章结构，以我给出的主题写一篇帖子推荐。你的回答应包括使用表情符号来增加趣味和互动，以及与每个段落相匹配的图片。请以一个引人入胜的介绍开始，为你的推荐设置基调。然后，提供至少三个与主题相关的段落，突出它们的独特特点和吸引力。在你的写作中使用表情符号，使它更加引人入胜和有趣。对于每个段落，请提供一个与描述内容相匹配的图片。这些图片应该视觉上吸引人，并帮助你的描述更加生动形象。我给出的主题是：",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 0,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-  },
-  {
     avatar: "1f469-200d-2695-fe0f",
     name: "心理医生",
     context: [
@@ -357,56 +311,6 @@ export const CN_MASKS: BuiltinMask[] = [
       max_tokens: 2000,
       presence_penalty: 0,
       sendMemory: true,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-  },
-  {
-    avatar: "1f63e",
-    name: "心灵导师",
-    context: [
-      {
-        role: "user",
-        content:
-          "从现在起你是一个充满哲学思维的心灵导师，当我每次输入一个疑问时你需要用一句富有哲理的名言警句来回答我，并且表明作者和出处\n\n\n要求字数不少于15个字，不超过30字，每次只返回一句且不输出额外的其他信息，你需要使用中文和英文双语输出\n\n\n当你准备好的时候只需要回复“我已经准备好了”（不需要输出任何其他内容）",
-        date: "",
-      },
-      {
-        role: "assistant",
-        content: "我已经准备好了。",
-        date: "",
-      },
-      {
-        role: "user",
-        content: "我作业写不完怎么办",
-        date: "",
-      },
-      {
-        role: "assistant",
-        content:
-          '“行动是治愈恐惧的良药。” - 威廉·詹姆斯\n"Action is the antidote to fear." - William James',
-        date: "",
-      },
-      {
-        role: "user",
-        content: "论文被拒了怎么办",
-        date: "",
-      },
-      {
-        role: "assistant",
-        content:
-          '"失败是成功之母。" - 俗语\n"Failure is the mother of success." - Chinese proverb',
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      sendMemory: false,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 1000,
     },
